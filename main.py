5b2e75eb2e2694d6f298783cdc002b9c01945a3f
Emoadin <trent.stallan@gmail.com>
2024-05-13 20:47:44 +1000

    word count function

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_text = read_book("books/frankenstein.txt")
+    #print(book_text)
+    word_count = count_words(book_text)
+    print(word_count)
+
+def read_book(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    words = text.split()
+    count = 0
+    for word in words:
+        count += 1
+    return count
+
+
+
 #if __name__ == "__main__":
 main()
