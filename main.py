df5bf1e51c5d6ba78e41dc496a2b4d540fff8c56
Emoadin <trent.stallan@gmail.com>
2024-05-16 07:53:48 +1000

    character count added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     book_text = read_book("books/frankenstein.txt")
     #print(book_text)
     word_count = count_words(book_text)
+    char_count = count_char(book_text)
+    print(char_count)
     print(word_count)
 
 def read_book(path):
@@ -15,6 +17,21 @@ def count_words(text):
         count += 1
     return count
 
+def count_char(text):
+    text_set = set()
+    text_dict = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        text_set.add(char)
+    text_set.remove(' ')
+    for char in text_set:
+        text_dict.update({char: 0})
+    for char in lower_text:
+        if char in text_dict:
+            text_dict[char] += 1
+        
+
+    return text_dict
 
 
 #if __name__ == "__main__":
